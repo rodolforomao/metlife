caef18e5435cfcb7ad83d7b1350e186c56218402
Jordan Fernandes <jordan_silva93@hotmail.com>
2019-08-01 20:46:47 -0300

    commit diario

---
+++ app/Http/Controllers/RelatorioController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Dadoscadastrai;
+use App\Dadosfamiliare;
+use App\Rendimentomensal;
+use App\Patrimonio;
+use App\Padrao_de_vida;
+use App\Educacao;
+use App\Saldoemprestimo;
+use App\Emprestimo;
+use App\Inssfgtsprevidenciaseguro;
+use App\Inssprevidenciacliente;
+use App\Insssegurocliente;
+use App\Planoprodutodesc;
+use DB;
+
+class EditarController extends Controller {
+
+    public function __construct() {
+        //$this->middleware('auth');
+    }
+
+    public function index(Request $request, $id) {
+        $dadosCadastrais = Dadoscadastrai::findOrFail($id);
+        $dadosFamiliarConjugue = Dadosfamiliare::where('idCliente', $id)->where('tipoFamiliar', 'Conjugue')->get();
+        $dadosFamiliarFilho = Dadosfamiliare::where('idCliente', $id)->where('tipoFamiliar', 'Filho')->get();
+        $dadosRendimentoPrincipal = Rendimentomensal::where('idCliente', $id)->where('tipoFamiliar', 'Principal')->get();
+        $dadosRendimentoConjugue = Rendimentomensal::where('idCliente', $id)->where('tipoFamiliar', 'Conjugue')->get();
+        $dadosPatrimonio = Patrimonio::where('idCliente', $id)->get();
+        $dadosPadraoVida = Padrao_de_vida::where('idCliente', $id)->get();
+        $dadosEducacao = Educacao::where('idCliente', $id)->get();
+        $dadosSaldoEmprestimo = Saldoemprestimo::where('idCliente', $id)->get();
+        $dadosEmprestimos = Emprestimo::where('idCliente', $id)->get();
+        $dadosFGTS_INSS_principal = Inssfgtsprevidenciaseguro::where('idCliente', $id)->where('tipoFamiliar', 'Principal')->get();
+        $dadosFGTS_INSS_conjugue = Inssfgtsprevidenciaseguro::where('idCliente', $id)->where('tipoFamiliar', 'Conjugue')->get();
+        $dadosPrevidencia = Inssprevidenciacliente::where('idCliente', $id)->get();
+        $dadosSeguro = Insssegurocliente::where('idCliente', $id)->get();
+        
+        $dadosPlanosPrincipal = Planoprodutodesc::
+                        select("descricao", "planoprodutos.id", "planoprodutodescs.id as idProduto", "vigencia", "prazo", "capital", "valor", "tipoFamiliar")->
+                        leftJoin('planoprodutos', function ($join) {
+                            $join->on('planoprodutos.idproduto', '=', 'planoprodutodescs.id')
+                            ->where('planoprodutos.tipoFamiliar', '=', 'Principal');
+                        })
+                        ->where('idCliente', $id)
+                        ->orwhereNull('idCliente')->get();
+        $dadosPlanoConjugue = Planoprodutodesc::
+                        select("descricao", "planoprodutos.id", "planoprodutodescs.id as idProduto", "vigencia", "prazo", "capital", "valor", "tipoFamiliar")->
+                        leftJoin('planoprodutos', function ($join) {
+                            $join->on('planoprodutos.idproduto', '=', 'planoprodutodescs.id')
+                            ->where('planoprodutos.tipoFamiliar', '=', 'Conjugue');
+                        })
+                        ->where('idCliente', $id)
+                        ->orwhereNull('idCliente')->get();
+
+        return view('dashboard.v2_edit', [
+            'dadoscadastrais' => $dadosCadastrais,
+            'dadosFamiliarConjugue' => $dadosFamiliarConjugue,
+            'dadosFamiliarFilho' => $dadosFamiliarFilho,
+            'dadosRendimentoPrincipal' => $dadosRendimentoPrincipal,
+            'dadosRendimentoConjugue' => $dadosRendimentoConjugue,
+            'dadosPatrimonio' => $dadosPatrimonio,
+            'dadosPadraoVida' => $dadosPadraoVida,
+            'dadosEducacao' => $dadosEducacao,
+            'dadosSaldoEmprestimo' => $dadosSaldoEmprestimo,
+            'dadosEmprestimos' => $dadosEmprestimos,
+            'dadosFGTS_INSS_principal' => $dadosFGTS_INSS_principal,
+            'dadosFGTS_INSS_conjugue' => $dadosFGTS_INSS_conjugue,
+            'dadosPrevidencia' => $dadosPrevidencia,
+            'dadosSeguro' => $dadosSeguro,
+            'dadosPlanosPrincipal' => $dadosPlanosPrincipal,
+            'dadosPlanoConjugue' => $dadosPlanoConjugue
+        ]);
+    }
+
+}
